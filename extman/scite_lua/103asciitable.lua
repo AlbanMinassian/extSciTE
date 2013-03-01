248a4538f486cb8fa8c55aa1d8796186206a2533
ami44 <alban.minassian@free.fr>
2013-03-01 15:45:14 +0100

    Afficher caractères spéciaux < 32 dans la console

--- extman/scite_lua/103asciitable.lua
+++ extman/scite_lua/103asciitable.lua
@@ -2,33 +2,50 @@
 
 -- -------------------------------------------------------------------------------------------------------
 -- http://lua-users.org/wiki/SciteAsciiTable
--- ASCII Table for SciTE; khman 20050812; public domain
---a adapter pour s'afficher dans la console et accélérer copier/coller ( editor:AddText à la place de editor:AddText)
+-- ASCII Table for SciTE
+-- khman 20050812; public domain
+-- Ben Fisher, 2006, Public domain
+-- adapté pour s'afficher dans la console et accélérer copier/coller 
 -- -------------------------------------------------------------------------------------------------------
 
 function ASCIITable()
-  local Ctrl = false -- set to true if ASCII<32 appear as valid chars
-  scite.Open("") -- <===  indispensable sion la console affiche en utf8
-  local hl = "    +----------------+\n"
-  editor:AddText("ASCII Table:\n"..hl.."Hex |0123456789ABCDEF|\n"..hl)
-  local start = Ctrl and 0 or 32
-  for x = start, 240, 16 do
-    editor:AddText(string.format(" %02X |", x))
-    for y = x, x+15 do editor:AddText(string.char(y)) end
-    editor:AddText("|\n")
-  end
-  editor:AddText(hl)
-  if not Ctrl then
-    editor:AddText(
-      "\nControl Characters:\n"..
-      " 00: NUL SOH STX ETX\n 04: EOT ENQ ACK BEL\n"..
-      " 08: BS  HT  LF  VT\n 0C: FF  CR  SO  SI\n"..
-      " 10: DLE DC1 DC2 DC3\n 14: DC4 NAK SYN ETB\n"..
-      " 18: CAN EM  SUB ESC\n 1C: FS  GS  RS  US\n"
-    )
-  end
-end
 
+     
+    local hl = "    +----------------+\n"
+    output:AddText("ASCII Table:\n"..hl.."Hex |0123456789ABCDEF|\n"..hl)
+    for x = 32, 240, 16 do
+        output:AddText(string.format(" %02X |", x))
+        for y = x, x+15 do output:AddText(string.char(y)) end
+        output:AddText("|\n")
+    end
+    output:AddText(hl)
+  
+    -- Afficher les caractères spéciaux
+    local padnum = function (nIn)
+        if nIn < 10 then return "00"..nIn
+        elseif nIn < 100 then return "0"..nIn
+        else return nIn
+        end
+    end
+    local overrides = { [0]="(Null)", [9]="(Tab)",[10]="(\\n Newline)", [13]="(\\r Return)", [32]="(Space)"}
+    local c
+    for n=0,31 do
+        if overrides[n] then c = overrides[n] else c = string.char(n) end
+        print (padnum(n).."  "..c)
+    end
+    
+    -- forcer la console à s'afficher nn mode 0 et pas utf8
+    -- Default (single byte character set)  0
+    -- UTF-8	65001
+    -- Japanese Shift-JIS	932
+    -- Simplified Chinese GBK	936
+    -- Korean Wansung	949
+    -- Traditional Chinese Big5	950
+    -- Korean Johab	1361
+    props['output.code.page']=0
+
+
+end
 
 idx =  33
 props['command.name.'..idx..'.*'] ="ASCII table"
